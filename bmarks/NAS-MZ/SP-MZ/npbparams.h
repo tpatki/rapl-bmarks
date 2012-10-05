3ed0cf4a444b34f3cf5ae41fd7a9eb81d1930f59
Barry Rountree <rountree4@llnl.gov>
2012-10-05 16:18:25 -0700

    rzmerl runs.

--- bmarks/NAS-MZ/SP-MZ/npbparams.h
+++ bmarks/NAS-MZ/SP-MZ/npbparams.h
@@ -34,7 +34,7 @@ c
         logical  convertdouble
         parameter (convertdouble = .false.)
         character compiletime*11
-        parameter (compiletime='04 Oct 2012')
+        parameter (compiletime='05 Oct 2012')
         character npbversion*5
         parameter (npbversion='3.3.1')
         character cs1*6
