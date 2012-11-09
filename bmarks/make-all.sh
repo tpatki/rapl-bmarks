18000bc0b89de93f14d89c6de8aa88794dc17e7b
Tapasya Patki <tpatki@email.arizona.edu>
2012-11-09 09:23:10 -0800

    new

--- bmarks/make-all.sh
+++ bmarks/make-all.sh
@@ -4,7 +4,7 @@
 curr=`pwd`
 
 #export PATH_LIBMSR=$curr/librapl/libmsr/lib
-export PATH_LIBMSR=$curr/librapl/libmsr/lib
+export PATH_LIBMSR=$curr/rapl-bmarks/bmarks/librapl/libmsr/lib
 echo $PATH_LIBMSR
 
 cd librapl/libmsr
