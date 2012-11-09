f29bb2985ab663d3e2367403ae9ae11992ea45ee
Tapasya Patki <patki1@llnl.gov>
2012-11-09 08:31:52 -0700

    New

--- bmarks/submit-small.sh
+++ bmarks/submit-small.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 cd NAS-MZ/SP-MZ/power
-#msub rapl-runs-small.msub
+msub rapl-runs-small.msub
 
-#cd ../../BT-MZ/power
-#msub rapl-runs-small.msub
+cd ../../BT-MZ/power
+msub rapl-runs-small.msub
 
 #cd ../../LU-MZ/power
 #msub rapl-runs-small.msub
 
 
-cd ../../../lulesh-oct30
-msub rapl-runs-small.msub
-
-#cd ../../sphot/power
+#cd ../../../lulesh-oct30
 #msub rapl-runs-small.msub
+
+cd ../../sphot/power
+msub rapl-runs-small.msub
 
 #cd ../../../librapl/libmsr/examples/spin-with-barrier/
 #msub rapl-runs-small.msub
