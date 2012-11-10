f56aa1fbf88b60fc36fc54fadfb3240f74ca3708
Tapasya Patki <patki1@llnl.gov>
2012-11-09 18:08:59 -0700

    fix??

--- bmarks/make-all.sh
+++ bmarks/make-all.sh
@@ -3,8 +3,8 @@
 #Export path to libmsr
 curr=`pwd`
 
-#export PATH_LIBMSR=$curr/librapl/libmsr/lib
-export PATH_LIBMSR=$curr/rapl-bmarks/bmarks/librapl/libmsr/lib
+export PATH_LIBMSR=$curr/librapl/libmsr/lib
+#export PATH_LIBMSR=$curr/rapl-bmarks/bmarks/librapl/libmsr/lib
 echo $PATH_LIBMSR
 
 cd librapl/libmsr
