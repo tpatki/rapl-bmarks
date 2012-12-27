f98d2368111f73bfa2500f23ac3d4db7f4b94bdf
Tapasya Patki <tpatki@email.arizona.edu>
2012-12-27 00:32:34 -0700

    Try using the non-turbo version of librapl.

--- bmarks/make-all.sh
+++ bmarks/make-all.sh
@@ -3,11 +3,11 @@
 #Export path to libmsr
 curr=`pwd`
 
-export PATH_LIBMSR=$curr/librapl/libmsr/lib
+export PATH_LIBMSR=$curr/librapl-master/libmsr/lib
 #export PATH_LIBMSR=$curr/rapl-bmarks/bmarks/librapl/libmsr/lib
 echo $PATH_LIBMSR
 
-cd librapl/libmsr
+cd librapl-master/libmsr
 mkdir -p lib
 make clean && make -j8 
 
