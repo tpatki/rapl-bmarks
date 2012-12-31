981fbf39a09fe3364a272a18c8e78c5d5f0a2f62
Barry Rountree <rountree4@llnl.gov>
2012-12-31 02:07:47 -0800

    Long night...

--- bmarks/NAS-MZ/SP-MZ/npbparams.h
+++ bmarks/NAS-MZ/SP-MZ/npbparams.h
@@ -1,4 +1,4 @@
-c NPROCS = 32 CLASS = C
+c NPROCS = 64 CLASS = C
 c  
 c  
 c  This file is generated automatically by the setparams utility.
@@ -8,7 +8,7 @@ c
         character class
         parameter (class='C')
         integer num_procs, num_procs2
-        parameter (num_procs=32, num_procs2=32)
+        parameter (num_procs=64, num_procs2=64)
         integer x_zones, y_zones
         parameter (x_zones=16, y_zones=16)
         integer gx_size, gy_size, gz_size, niter_default
@@ -18,13 +18,13 @@ c
         parameter (problem_size = 30)
         integer max_xysize, max_xybcsize
         integer proc_max_size, proc_max_size5, proc_max_bcsize
-        parameter (max_xysize=4960)
-        parameter (max_xybcsize=3760)
+        parameter (max_xysize=2480)
+        parameter (max_xybcsize=1880)
         parameter (proc_max_size=max_xysize*gz_size)
         parameter (proc_max_size5=proc_max_size*5)
         parameter (proc_max_bcsize=max_xybcsize*(gz_size-2))
         integer max_numzones
-        parameter (max_numzones=8)
+        parameter (max_numzones=4)
         double precision dt_default, ratio
         parameter (dt_default = 0.00067d0, ratio = 1.d0)
         integer start1, start5, qstart_west, qstart_east
@@ -34,7 +34,7 @@ c
         logical  convertdouble
         parameter (convertdouble = .false.)
         character compiletime*11
-        parameter (compiletime='28 Dec 2012')
+        parameter (compiletime='30 Dec 2012')
         character npbversion*5
         parameter (npbversion='3.3.1')
         character cs1*6
