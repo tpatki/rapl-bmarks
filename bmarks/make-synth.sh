aad3a4773afb031134b5cf88b33f0185de64daae
Tapasya Patki <tpatki@email.arizona.edu>
2013-01-04 04:50:07 -0700

    rerun synths without library overhead

--- bmarks/make-synth.sh
+++ bmarks/make-synth.sh
@@ -14,27 +14,24 @@ make clean && make
 cd mpi
 make clean && make rapl-icc
 
-#cd ../../../lulesh-oct30
-#make clean && make
-
-#cd ../examples/spin-with-barrier
-#make clean && make
-#cd ../../../
-
-#cd lulesh
-#make clean && make
-
 cd ../../../synthetic/no-scale-mem-comm
 make clean && make -j8
 
-#cd ../no-scale-mem
-#make clean && make -j8
-
-#cd ../scale-cpu
-#make clean && make -j8
-
 cd ../no-scale-cpu-comm
 make clean && make -j8
+
+cd ../no-scale-mem
+make clean && make -j8
+
+cd ../no-scale-cpu
+make clean && make -j8
+
+cd ../scale-cpu
+make clean && make -j8
+
+cd ../scale-mem
+make clean && make -j8
+
 
 #cd ../turbo-tests
 #make clean && make -j8
--- bmarks/submit-synth.sh
+++ bmarks/submit-synth.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#cd synthetic/no-scale-cpu
-#msub rapl-runs-small.msub
+cd synthetic/no-scale-cpu
+msub rapl-runs-small.msub
 
-#cd ../scale-cpu
-#msub rapl-runs-small.msub
+cd ../scale-cpu
+msub rapl-runs-small.msub
 
-#cd synthetic/scale-mem
-#msub rapl-runs-small.msub
+cd ../scale-mem
+msub rapl-runs-small.msub
 
-cd synthetic/no-scale-mem-comm
+cd ../no-scale-mem
 msub rapl-runs-small.msub
 
 
@@ -17,8 +17,8 @@ cd ../no-scale-cpu-comm
 msub rapl-runs-small.msub
 
 
-#cd ../scale-cpu
-#msub rapl-runs-small.msub
+cd ../no-scale-mem-comm
+msub rapl-runs-small.msub
 
 #cd ../turbo-tests
 #msub script.msub
